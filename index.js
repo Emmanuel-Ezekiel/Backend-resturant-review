b57584f01795f9b1003ed10be48aaf650d13f98c
Emmanuel-EEzekiel <emamex44@gmail.com>
2021-11-24 10:39:06 +0100

    add file

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import app from "./server.js"
 import mongodb from "mongodb"
 import dotenv from "dotenv"
+import RestaurantsDAO from "./deo/restaurantDAO.js"
 
 dotenv.config()
 
@@ -22,6 +23,7 @@ MongoClient.connect(
     process.exit(1)
 })
 .then (async client => {
+    await RestaurantsDAO.injectDB(client)
     app.listen(port, () => {
         console.log(`listen on port ${port}`)
     })
